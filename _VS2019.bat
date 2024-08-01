ab5485aa0b64ba38d192ec2dff241e3ac18e202b
Toshiaki Kawabata <toshiaki@techsoft3d.com>
2024-08-01 09:36:10 +0900

    Ported 2024.5.0

--- _VS2019.bat
+++ _VS2019.bat
@@ -1,5 +1,5 @@
-SET HVISUALIZE_INSTALL_DIR=C:\SDK\Visualize\HOOPS_Visualize_2024.2.0
-SET HLUMINATE_INSTALL_DIR=C:\SDK\Luminate\HOOPS_Luminate_2024.1.0
-SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2024.2.0
+SET HVISUALIZE_INSTALL_DIR=C:\SDK\Visualize\HOOPS_Visualize_2024.5.0
+SET HLUMINATE_INSTALL_DIR=C:\SDK\Luminate\HOOPS_Luminate_2024.5.0
+SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2024.5.0
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" hps_mfc_sandbox_v142.sln
